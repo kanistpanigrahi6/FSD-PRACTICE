8cc78e669207e9818139088e9ee083a92d659abe
Harshitkumar Panigrahi <kanistpanigrahi6@gmail.com>
2025-09-03 20:52:40 +0530

    Create practice express lib ,with get and post.js

---
+++ practice express lib ,with get and post.js
@@ -0,0 +1,28 @@
+const express=require("express")
+var app=express();
+const port=3000
+
+function calculate(counter){
+    var sum=0
+    for(let i=0;i<counter;i++){
+        sum=sum+i
+    }
+    return sum;
+}
+
+function handlefirstrequest(req,res){
+    var calculatedsum=calculate(99);
+    console.log(calculatedsum)
+    var answer="the sum is"+calculatedsum
+    res.send(answer)
+}
+app.get(`/`,handlefirstrequest)
+
+function started(){
+    console.log(`Example app listening on port ${port}`)
+}
+app.listen(port,started)
+
+
+
+
