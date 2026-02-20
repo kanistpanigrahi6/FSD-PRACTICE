9de97c1a8619ff6a6af764a02e9b09328d769b91
Harshitkumar Panigrahi <kanistpanigrahi6@gmail.com>
2026-02-20 11:22:03 +0530

    Set up Express app with status code handling
    Initialize an Express app with body-parser and handle POST requests with status codes.

---
+++ initialize express app and use status codes.js
@@ -0,0 +1,45 @@
+const express = require("express");
+var bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+
+// function middleware1(req, res, next){
+//     console.log("from inside a middleware " + req.headers.cnt);
+//     next();
+// }
+// app.use(middleware1);
+
+app.use(bodyParser.json());
+
+function calculate(cnt){
+    var sum = 0;
+    for(var i = 0; i <= cnt; i++){
+        sum = sum + i;
+    }
+    return sum;
+}
+
+function handlesfirstrequest(req, res){
+    // console.log(req.body)
+    var cnt = req.body.cnt; 
+    // var cnt =req.headers.cnt;  
+    if (cnt<1000){
+        var calculatedsum = calculate(cnt);
+        console.log(calculatedsum);
+        var answer = "The sum is " + calculatedsum;
+        res.status(200).send(answer);
+}
+else{
+    res.status(400).send("u have sent a large number, please send a smaller number");
+}
+    
+}
+
+app.post("/createuser", handlesfirstrequest);
+
+function started(){
+    console.log(`App listening on port ${port}`);
+}
+
+app.listen(port, started);
